26af5a45fb88a9714ef059804668d064544a5555
Armando Hernandez <120071751+armandovh04@users.noreply.github.com>
2024-12-11 16:21:12 -0600

    Update page.h

--- page.h
+++ page.h
@@ -1,1 +1,15 @@
+#ifndef PAGE_H
+#define PAGE_H
 
+// In here I will use the a structure for tracking physical pages in memory
+struct ppage {
+  struct ppage *next;           // Pointer to the next physical page
+  struct ppage *prev;           // Pointer to the previous physical page
+  void *physical_addr;          // Pointer to the physical address of the page
+};
+
+void init_pfa_list(void);                                
+struct ppage *allocate_physical_pages(unsigned int npages); 
+void free_physical_pages(struct ppage *ppage_list);      
+
+#endif 
